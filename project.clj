8d4484cee8aa77e6703a09ed2bbd046a872fd04b
Stathis Sideris <sideris@gmail.com>
2014-08-28 23:43:50 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject retrograde "0.9"
+(defproject retrograde "0.10"
   :description "Retrograde is a small Clojure DSL that deals with the problem of accessing information and results from previous iterations during the **lazy** transformation of a sequence or during the **lazy** generation of an infinite sequence."
   :url "https://github.com/stathissideris/retrograde"
   :license {:name "Eclipse Public License"
